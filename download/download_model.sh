9aed1dfd4c54734c20e5b80d061f97bc02177b4d
zengxianyu <zengxianyu18@gmail.com>
2022-01-20 08:25:43 +0800

    downm

---
+++ download/download_model.sh
@@ -0,0 +1,8 @@
+wget https://maildluteducn-my.sharepoint.com/:u:/g/personal/zengyu_mail_dlut_edu_cn/ETFFjNQcRzFMs2Dgq6tiVnIB3msKBXYMBOSWnVYEHgrlxQ?download=1
+mv ETFFjNQcRzFMs2Dgq6tiVnIB3msKBXYMBOSWnVYEHgrlxQ?download=1 ./checkpoints/celeb.zip
+wget https://maildluteducn-my.sharepoint.com/:u:/g/personal/zengyu_mail_dlut_edu_cn/EVjX29v1-cVKh_NYo06AbUsBX1ufD0wsb9cLWQsBLGYKDw?download=1
+mv EVjX29v1-cVKh_NYo06AbUsBX1ufD0wsb9cLWQsBLGYKDw?download=1 ./checkpoints/places.zip
+cd checkpoints
+unzip celeb.zip
+unzip places.zip
+cd ..
